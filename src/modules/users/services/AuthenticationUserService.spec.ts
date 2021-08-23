6683adb0031e59b0e5da187e31086460c2b7f439
Ramon Rangel Lima <ramon.lima@programmers.com.br>
2021-08-23 10:15:24 -0300

    Testes de Autenticação

--- src/modules/users/services/AuthenticationUserService.spec.ts
+++ src/modules/users/services/AuthenticationUserService.spec.ts
@@ -1,8 +1,8 @@
-import AppError from "@shared/errors/AppError";
 import FakeUsersRepository from "../repositories/fakes/FakeUsersRepository";
 import AuthenticationUserService from "./AuthenticationUserService";
 import CreateUserService from "./CreateUserService";
 import FakeHashProvider from "../providers/HashProvider/fakes/FakeHashProvider";
+import AppError from "@shared/errors/AppError";
 
 describe('AuthenticationUser', () => {
     it('should be able to authenticate', async () => {
@@ -31,4 +31,43 @@ describe('AuthenticationUser', () => {
         expect(response.user).toEqual(user);
     });
 
+    it('should not be able to authenticate with non existing', async () => {
+      const fakeUsersRepository = new FakeUsersRepository();
+      const fakeHashProvider = new FakeHashProvider();
+
+      const authenticateUser = new AuthenticationUserService(
+        fakeUsersRepository,
+        fakeHashProvider
+       );
+
+       expect(
+        authenticateUser.execute({
+        email: "fulano@silva@gmail.com",
+        password: "123456"
+      })).toBeInstanceOf(AppError);
+   });
+
+   it('should not be able to authenticate with unexisting password', async () => {
+       const fakeUsersRepository = new FakeUsersRepository();
+       const fakeHashProvider = new FakeHashProvider();
+
+       const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+       const authenticateUser = new AuthenticationUserService(
+         fakeUsersRepository,
+         fakeHashProvider
+        );
+
+        await createUser.execute({
+            name: "Fulano Silva",
+            email: "fulano@silva@gmail.com",
+            password: "123456"
+        });
+
+        expect(
+          await authenticateUser.execute({
+            email: "fulano@silva@gmail.com",
+            password: "654321"
+          })
+        ).toBeInstanceOf(AppError);
+  });
 });
